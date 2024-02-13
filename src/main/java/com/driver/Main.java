bc6728a384eec6a1b686b01b94b1ad24222fc61b
pintu_giri <mrpintugiri@gmail.com>
2024-02-13 23:01:16 +0530

    First try

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj=new RWOnly();
+        obj.setName("Pintu");
+        System.out.println(obj.getName());
+
+        // error- java: name has private access in com.driver.RWOnly
+
+    }
 }
